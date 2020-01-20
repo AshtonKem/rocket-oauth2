8806a72833e04bce64b5075d20e9c04e2b891e79
Ashton Kemerling <ashtonkemerling@gmail.com>
2020-01-20 09:13:55 -0800

    Fairly basic attempt to get access tokens.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use crate::OAuthError::Unauthenticated;
 use rocket::http::Status;
 use rocket::request::FromRequest;
 use rocket::{Outcome, Request};
+use std::collections::HashMap;
 
 pub struct OAuthConfiguration {
     pub client_id: String,
@@ -29,4 +30,20 @@ impl<'a, 'r> FromRequest<'a, 'r> for OAuthUser {
             }),
         }
     }
+}
+
+pub fn get_access_token(config: &OAuthConfiguration, code: String) -> String {
+    let resp: HashMap<String, String> = reqwest::get("https://oauth2.googleapis.com/token")
+        .query(&[
+            ("code", code),
+            ("client_id", config.client_id),
+            ("client_secret", config.client_secret),
+            ("grant_type", "authorization_code"),
+            ("redirect_url", "https://example.com/oauth"),
+        ])
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?;
+
+    resp.get("access_token")?.to_string()
 }
