4f8f3d07006422492f367c9221aabe48518f0aa7
Ashton Kemerling <ashtonkemerling@gmail.com>
2020-01-20 09:31:36 -0800

    Big mess of Reqwest & Rocket to handle scopes.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,13 @@
-use crate::OAuthError::Unauthenticated;
 use rocket::http::Status;
 use rocket::request::FromRequest;
+use rocket::response::Redirect;
 use rocket::{Outcome, Request};
 use std::collections::HashMap;
 
 pub struct OAuthConfiguration {
     pub client_id: String,
     pub client_secret: String,
+    pub redirect_uri: String,
     pub extra_scopes: Vec<String>,
 }
 
@@ -32,18 +33,27 @@ impl<'a, 'r> FromRequest<'a, 'r> for OAuthUser {
     }
 }
 
-pub fn get_access_token(config: &OAuthConfiguration, code: String) -> String {
-    let resp: HashMap<String, String> = reqwest::get("https://oauth2.googleapis.com/token")
+pub fn redirect_to_oauth(config: &OAuthConfiguration) -> Redirect {
+    Redirect::to(format!("https://accounts.google.com/o/oauth2/v2/auth?scope={}&access_type=offline&include_granted_scopes=true&state=state_parameter_passthrough_value&redirect_uri={}&response_type=code&client_id=${}",
+                         "fake-scope", config.redirect_uri, config.client_id))
+}
+
+pub async fn get_access_token(config: &OAuthConfiguration, code: String) -> String {
+    let resp: HashMap<String, String> = reqwest::Client::new()
+        .get("https://oauth2.googleapis.com/token")
         .query(&[
             ("code", code),
-            ("client_id", config.client_id),
-            ("client_secret", config.client_secret),
-            ("grant_type", "authorization_code"),
-            ("redirect_url", "https://example.com/oauth"),
+            ("client_id", config.client_id.to_string()),
+            ("client_secret", config.client_secret.to_string()),
+            ("grant_type", "authorization_code".to_string()),
+            ("redirect_url", "https://example.com/oauth".to_string()),
         ])
-        .await?
+        .send()
+        .await
+        .unwrap()
         .json::<HashMap<String, String>>()
-        .await?;
+        .await
+        .unwrap();
 
-    resp.get("access_token")?.to_string()
+    resp.get("access_token").unwrap().to_string()
 }
