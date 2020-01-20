f537fc87e0c2a728602d2192f604158002345afa
Ashton Kemerling <ashtonkemerling@gmail.com>
2020-01-20 09:02:15 -0800

    Beginnings of some OAuth classes.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,32 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use crate::OAuthError::Unauthenticated;
+use rocket::http::Status;
+use rocket::request::FromRequest;
+use rocket::{Outcome, Request};
+
+pub struct OAuthConfiguration {
+    pub client_id: String,
+    pub client_secret: String,
+    pub extra_scopes: Vec<String>,
+}
+
+pub struct OAuthUser {
+    pub access_token: String,
+}
+
+#[derive(Debug)]
+pub enum OAuthError {
+    Unauthenticated,
+}
+
+impl<'a, 'r> FromRequest<'a, 'r> for OAuthUser {
+    type Error = OAuthError;
+
+    fn from_request(request: &'a Request<'r>) -> Outcome<Self, (Status, Self::Error), ()> {
+        match request.cookies().get_private("access_token") {
+            None => Outcome::Failure((Status::Unauthorized, Unauthenticated)),
+            Some(cookie) => Outcome::Success(OAuthUser {
+                access_token: cookie.to_string(),
+            }),
+        }
     }
 }
