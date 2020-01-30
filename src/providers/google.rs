a4a83e5fc3b782a461376c66c8b6e22fd9c5330a
Ashton Kemerling <ashtonkemerling@gmail.com>
2020-01-29 18:48:33 -0800

    Tests for redirect url.

--- src/providers/google.rs
+++ src/providers/google.rs
@@ -40,3 +40,40 @@ impl OauthProvider for GoogleProvider {
         response.unwrap().json::<TokenMessage>()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::providers::{GoogleProvider, OauthProvider};
+
+    #[test]
+    fn test_redirect() {
+        let provider = GoogleProvider {};
+        let scopes: Vec<&str> = vec![];
+        let expected = "https://accounts.google.com/o/oauth2/v2/auth?scope=profile&access_type=offline&include_granted_scopes=true&redirect_uri=redirect&response_type=code&client_id=client_id&state=original";
+        assert_eq!(
+            expected,
+            provider.get_redirect(
+                &"redirect".to_string(),
+                &"original".to_string(),
+                &"client_id".to_string(),
+                &scopes
+            )
+        );
+    }
+
+    #[test]
+    fn test_redirect_with_scopes() {
+        let provider = GoogleProvider {};
+        let scopes: Vec<&str> = vec!["fake-scope"];
+        let expected = "https://accounts.google.com/o/oauth2/v2/auth?scope=profile%20fake-scope&access_type=offline&include_granted_scopes=true&redirect_uri=redirect&response_type=code&client_id=client_id&state=original";
+        assert_eq!(
+            expected,
+            provider.get_redirect(
+                &"redirect".to_string(),
+                &"original".to_string(),
+                &"client_id".to_string(),
+                &scopes
+            )
+        );
+    }
+}
